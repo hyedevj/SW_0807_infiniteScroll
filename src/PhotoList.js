7cc93a600b9d1d9f9dcf0139b40639aa7c3f69b9
hyedevj <hyedevj@gmail.com>
2023-08-08 17:31:27 +0900

    작업중

---
+++ src/PhotoList.js
@@ -0,0 +1,63 @@
+/* initialState: [
+    {
+        id: 1,
+        imagePath: ''
+    }
+]
+
+*/
+
+export default function PhotoList({ $target, initialState, onScrollEnded }) {
+    let isInitialState = false
+
+    const $photolist = document.createElement('div')
+    $target.appendChild($photolist)
+    this.state = initialState
+
+    this.setState = nextState => {
+        this.state = nextState
+        this.render()
+    }
+
+    this.render = () => {
+        if (!isInitialState) {
+            $photolist.innerHTML = `
+                <ul class="PhotoList_photos"></ul>
+                <button class="PhotoList_loadModre" style="width: 100%; height: 200px; font-size: 20px">Load More</button>
+            `
+            isInitialState = true
+        }
+        const { photos } = this.state
+        const $photos = $photolist.querySelector('.PhotoList_photos')
+
+        photos.forEach(photo => {
+            // photo의 id 기준으로 렌더링이 되어있는지 체크
+            if ($photos.querySelector(`li[data-id="${photo.id}"]`) === null) {
+                // 없으면 li 생성하고 $photos에 appendChild
+                const $li = document.createElement('li')
+                $li.setAttribute('data-id', photo.id)
+                $li.style = 'list-style: none;'
+                $li.innerHTML = `<img width="100%" src="${photo.imagePath}" />`
+
+                $photos.appendChild($li)
+            }
+        })
+    }
+
+    this.render()
+
+    $photolist.addEventListener('click', e => {
+        if (e.target.className === 'PhotoList_loadModre' && !this.state.isLoading) {
+            onScrollEnded()
+        }
+    })
+
+    window.addEventListener('scroll', () => {
+        const { isLoading, totalCount, photos } = this.state
+        const isScrollEnded = ((window.innerHeight + window.scrollY) + 100 >= document.body.offsetHeight)
+
+        if (isScrollEnded && !isLoading && photos.length < totalCount) {
+            onScrollEnded()
+        }
+    })
+}
